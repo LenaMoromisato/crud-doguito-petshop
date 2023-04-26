9c4f3ab856a7ea946ba0639608260879c81cd8ba
LenaMoromisato <moromisato.lena@gmail.com>
2023-04-26 18:26:48 -0300

    lidando com promises

--- admin/service/cliente-service.js
+++ admin/service/cliente-service.js
@@ -17,31 +17,27 @@ const criaNovaLinha = (nome, email) => {
 
 const tabela = document.querySelector('[data-tabela]');
 
-const http = new XMLHttpRequest();
+const listaClientes = () => {
+    const promise = new Promise((resolve, reject) => {
+        const http = new XMLHttpRequest();
 
-http.open('GET', 'http://localhost:3000/profile');
+        http.open('GET', 'http://localhost:3000/profile');
 
-http.send();
-
-http.onload = () => {
-    const data = JSON.parse(http.response);
-    
-    data.forEach(elemento => {
-        tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email));
+        http.onload = () => {
+            if(http.status >= 400) {
+                reject(JSON.parse(http.response))
+            } else {
+                resolve(JSON.parse(http.response))
+            }
+        }
+        http.send();
     })
-    
-    /* caso tenha dados antigos
-    const http2 = new XMLHttpRequest();
-
-    http2.open('GET', 'http://localhost:3000/profile/semanaPassada');
-
-    http2.onload = () => {
-        const data = JSON.parse(http.response);
-    
-        data.forEach(elemento => {
-        tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email));
-        })
-    }
-    http2.send()
-    */
+    return promise;
 }
+
+listaClientes()
+.then(data => {
+    data.forEach(elemento => {
+        tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email))
+    })
+})
