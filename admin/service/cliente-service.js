e7df5728a7270b654c2fc5784a549a21123b308a
LenaMoromisato <moromisato.lena@gmail.com>
2023-04-28 10:35:27 -0300

    deletando um cliente

--- admin/service/cliente-service.js
+++ admin/service/cliente-service.js
@@ -21,9 +21,16 @@ const criaCliente = (nome, email) => {
     })
 }
 
+const removeCliente = (id) => {
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: 'DELETE'
+    })
+}
+
 export const clienteService = {
     listaClientes,
-    criaCliente
+    criaCliente,
+    removeCliente
 }
 
 
