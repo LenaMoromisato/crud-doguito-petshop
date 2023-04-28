87c645224d8a53a03ae83f57d2f487eb0ba2ebf7
LenaMoromisato <moromisato.lena@gmail.com>
2023-04-28 18:25:12 -0300

    lidando com erros

--- admin/controller/atualizaCliente-controller.js
+++ admin/controller/atualizaCliente-controller.js
@@ -8,18 +8,30 @@ import { clienteService } from "../service/cliente-service.js";
     const inputNome = document.querySelector('[data-nome]');
     const inputEmail = document.querySelector('[data-email]');
 
-    const dados = await clienteService.detalhaCliente(id)
-    inputNome.value = dados.nome
-    inputEmail.value = dados.email
+    try {
+        const dados = await clienteService.detalhaCliente(id)
+        inputNome.value = dados.nome
+        inputEmail.value = dados.email
+    }
+    catch(erro) {
+        console.log(erro)
+        window.location.href = '../telas/erro.html'
+    }
 
     const formulario = document.querySelector('[data-form]');
 
     formulario.addEventListener('submit', async (evento) => {
         evento.preventDefault()
-
-        await clienteService.atualizaCliente(id, inputNome.value, inputEmail.value)
         
-        window.location.href = "../telas/edicao_concluida.html"   
+        try {
+            await clienteService.atualizaCliente(id, inputNome.value, inputEmail.value)
+            
+            window.location.href = "../telas/edicao_concluida.html" 
+        }
+        catch(erro) {
+            console.log(erro)
+            window.location.href = '../telas/erro.html'
+        }
     })
 })()
 
--- admin/service/cliente-service.js
+++ admin/service/cliente-service.js
@@ -1,7 +1,10 @@
 const listaClientes = () => {
     return fetch(`http://localhost:3000/profile`)
     .then(resposta => {
-        return resposta.json()
+        if(resposta.ok){
+            return resposta.json()
+        }
+        throw new Error('Não foi possível listar os clientes')
     })
 }
 
@@ -17,20 +20,28 @@ const criaCliente = (nome, email) => {
         })
     })
     .then(resposta => {
-        return resposta.body
+        if(resposta.ok) {
+           return resposta.body 
+        }
+        throw new Error('Não foi possível criar um cliente')
     })
 }
 
 const removeCliente = (id) => {
     return fetch(`http://localhost:3000/profile/${id}`, {
         method: 'DELETE'
+    }).then(resposta => {
+        throw new Error('Não foi possível remover um cliente')
     })
 }
 
 const detalhaCliente = (id) => {
     return fetch(`http://localhost:3000/profile/${id}`)
     .then(resposta => {
-        return resposta.json()
+        if(resposta.ok) {
+            return resposta.json()
+        }
+        throw new Error('Não foi possível detalhar o cliente')
     })
 }
 
@@ -46,7 +57,10 @@ const atualizaCliente = (id, nome, email) => {
         })
     })
     .then(resposta => {
-        return resposta.json()
+        if(resposta.ok) {
+            return resposta.json()
+        }
+        throw new Error('Não foi possível atualizar um cliente')
     })
 }
 
